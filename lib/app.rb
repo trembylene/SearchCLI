f0252dc61a6e0acabd808ff43cd3bddc2808a40e
Jessica Scanlan <jessica.scanlan.1993@gmail.com>
2018-10-07 08:08:52 +1100

    set up initial app and router file

---
+++ lib/app.rb
@@ -0,0 +1,7 @@
+# Load required files
+require_relative 'router'
+
+router = Router.new
+
+# Start the app
+router.run
---
+++ lib/router.rb
@@ -0,0 +1,39 @@
+class Router
+    def initialize
+        @running = true
+    end
+
+    def run
+        puts "Welcome to the Search CLI!"
+        puts "           --             "
+
+        while @running
+            display_menu
+            action = gets.chomp.to_i
+            print `clear`
+            route_action(action)
+        end
+    end
+
+    private
+
+    def route_action(action)
+        case action
+        # when 1 then @controller.search
+        when 0 then stop
+        else
+            puts "Please press 1 or 0"
+        end
+    end
+
+    def stop
+        @running = false
+    end
+
+    def display_menu
+        puts ""
+        puts "What do you want to do next?"
+        puts "1 - Search all data"
+        puts "0 - Stop and exit the program"
+    end
+end
