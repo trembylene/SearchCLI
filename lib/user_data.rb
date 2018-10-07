e870b0ef684252715a894f6b636756631c9067e4
Jessica Scanlan <jessica.scanlan.1993@gmail.com>
2018-10-07 18:58:02 +1100

    tidied user files

--- lib/user_data.rb
+++ lib/user_data.rb
@@ -1,7 +1,7 @@
 # Load required libraries
 require 'json'
 
-# This is the class that acts as the pseudo model/data for the data
+# This class acts as the pseudo model/database for User information
 class UserData
     attr_reader :data_hash
 
@@ -15,9 +15,5 @@ class UserData
 
     def load_user_file
         @data_hash = JSON.parse(@user_file)
-        # .with_indifferent_access
-        
-        # format to get results: @data_hash[0]["url"]
-        # format to get results: @data_hash[0].url
     end
 end
--- lib/users_controller.rb
+++ lib/users_controller.rb
@@ -1,8 +1,8 @@
 require_relative 'view'
 require_relative 'search'
 
-# This class acts as the controller
-class UserController
+# This class acts as the controller for User data
+class UsersController
     def initialize(user_data)
         @user_data = user_data
         @view = View.new
@@ -11,16 +11,11 @@ class UserController
     def search
         # Get search term from user
         search_term = @view.ask_search_term
-
         # Get list of all data
         all_data = @user_data.data_hash
-
         # Use the search term to search the data for a match
         matched_results = search_json_data(search_term, all_data)
-        # byebug
-        # Display the results to the user
+        # Display the results to the terminal
         @view.list_search_results(matched_results, search_term)
-
-        # Optional: Give the user another option to show more details for results (ie click 1 for more info on 1)
     end
 end
