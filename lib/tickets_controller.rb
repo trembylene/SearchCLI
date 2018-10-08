4ef799570645c5d02c3c9710359cfb0c7af544bc
Jessica Scanlan <jessica.scanlan.1993@gmail.com>
2018-10-09 07:10:31 +1100

    updated core files to include passing tickets

--- lib/tickets_controller.rb
+++ lib/tickets_controller.rb
@@ -4,15 +4,20 @@
 # This class acts as the controller for Ticket data
 class TicketsController
   def initialize(ticket_data)
-    @ticket_data = ticket_data
+    @ticket_data = ticket_data.ticket_data_hash
     # @view = View.new
+  end
+
+  def list
+    @ticket_data
   end
 
   def search
     # Get search term from ticket
     # search_term = @view.ask_search_term
     # Get list of all data
-    all_data = @ticket_data.ticket_data_hash
+    
+    
     # Use the search term to search the data for a match
     matched_results = search_json_data(search_term, all_data)
     # Display the results to the terminal
