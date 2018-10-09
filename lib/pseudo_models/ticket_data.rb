f092c87134db3fed0640a542c6e7f61cbc169bd4
Jessica Scanlan <jessica.scanlan.1993@gmail.com>
2018-10-09 21:06:18 +1100

    added comments for data

--- lib/pseudo_models/ticket_data.rb
+++ lib/pseudo_models/ticket_data.rb
@@ -1,11 +1,14 @@
 # Load required libraries
 require 'json'
 
-# This class acts as the pseudo model/database for Ticket information
+# This class acts as the pseudo model/database for Ticket 
+# information, by parsing json into ruby hash upon app
+# load, and by making this data available to the controller
 class TicketData
   attr_reader :ticket_data_hash
 
   def initialize(ticket_file)
+    # loads necessary data for the TicketData class
     @ticket_file = ticket_file
     @ticket_data_hash = []
     load_ticket_file
@@ -14,6 +17,7 @@ class TicketData
   private
 
   def load_ticket_file
+    # parses json
     @ticket_data_hash = JSON.parse(@ticket_file)
   end
 end
--- lib/pseudo_models/user_data.rb
+++ lib/pseudo_models/user_data.rb
@@ -1,11 +1,14 @@
 # Load required libraries
 require 'json'
 
-# This class acts as the pseudo model/database for User information
+# This class acts as the pseudo model/database for User 
+# information, by parsing json into ruby hash upon app
+# load, and by making this data available to the controller
 class UserData
   attr_reader :user_data_hash
 
   def initialize(user_file)
+    # loads necessary data for the UserData class
     @user_file = user_file
     @user_data_hash = []
     load_user_file
@@ -14,6 +17,7 @@ class UserData
   private
 
   def load_user_file
+    # parses json
     @user_data_hash = JSON.parse(@user_file)
   end
 end
